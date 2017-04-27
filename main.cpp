4202ec290e173fc1eb494f5389959b039944d27c
Albán <albanaguilar1@gmail.com>
2017-04-26 23:52:45 -0500

    Sexto avance

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@ int main( ){
     Reserva reservacion;
     cliente arregloPersonas[20];
     Reserva arregloReservaciones[20];
-    Automovil aAuto[20];
+    //Automovil aAuto[20];
 
-    int iContador = 0, iOpcion, iEdad, iTelefono, iCont = 0;;
+    int iContador = 0, iOpcion, iEdad, iTelefono, iCont = 0, resrvacionesContador = 0;
 
     string sNombre, sCorreoElectronico, sLinea;
     string arregloString[20];
@@ -21,6 +21,7 @@ int main( ){
     ifstream entradaArchivoReserva;
     entradaArchivoReserva.open("Carros.txt");
 
+    cout << endl;
     while ( !entradaArchivoReserva.eof() ){
         getline(entradaArchivoReserva, sLinea );
         arregloString[iCont] = sLinea;
@@ -30,20 +31,24 @@ int main( ){
 
     cout << endl;
     cout << endl;
-    cout << "Teclea el numero de opcion: " << endl;
+    cout << "Menu" << endl;
     cout << "   1.- Reservar un vehiculo" << endl;
     cout << "   2.- Consultar reservas" << endl;
+    cout << "Teclea el numero de opcion: ";
     cin >> iOpcion;
+    cout << endl;
 
+    cin.ignore();
     if ( iOpcion == 1){
         cout << "Provee los datos siguientes: " << endl;
-        cout << "   Nombre: " << endl;
+        cout << "   Nombre: ";
         getline(cin, sNombre);
-        cout << "   Edad: " << endl;
+        cout << "   Edad: ";
         cin >> iEdad;
-        cout << "   Telefono: " << endl;
+        cout << "   Telefono: ";
         cin >> iTelefono;
-        cout << "   Correo electronico: " << endl;
+        cin.ignore();
+        cout << "   Correo electronico: ";
         cin >> sCorreoElectronico;
 
         arregloPersonas[iContador].setNombre( sNombre);
@@ -51,14 +56,31 @@ int main( ){
         arregloPersonas[iContador].setTelefono(iTelefono);
         arregloPersonas[iContador].setCorreoElectronico(sCorreoElectronico);
 
-        for ( int iA = 0; iA < 20; iA++ ){
+        cout << "Marca " << "Modelo " << "Categoria " <<"Transmicion " << "Ubicacion " << "Capacidad " << "Costo de renta" << endl;
+        for ( int iA = 1; iA < 20; iA++ ){
             cout << iA << ".- " << arregloString[iA] << endl;
         }
 
+        cin.ignore();
         int numAuto;
-        cout << "Seleccione el numero del auto que guste: " <<endl;
+        cout << "Seleccione el numero del auto que guste: ";
         cin >>numAuto;
-        //if ( )
+
+        int sA = arregloString[numAuto];
+        while ( sA != ' ' ){
+
+        }
+        resrvacionesContador++;
+
+        cout << "!Se ha realizado su reserva exitosamente!" << endl;
+        //iContador = iContador + 1;
+
+    }
+    else if (iOpcion == 2){
+        cout <<"Reservas" << endl;
+        for (int iA = 0; iA < iContador; iA++){
+            cout << ( iA + 1 ) << ".- " << arregloReservaciones[resrvacionesContador] << endl;
+        }
     }
 
 
